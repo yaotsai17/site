050c9a740670b606ee7519ac0c20d7d8490598d4
yaotsai17 <yaotsai17@gmail.com>
2019-02-17 06:19:48 -0800

    adjusted intro image to use flexbox

--- src/component/LandingSplash/LandingSplash.component.js
+++ src/component/LandingSplash/LandingSplash.component.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 
 /** Local CSS */
 import './LandingSplash.component.css';
+import IntroImage from '../../img/bg.png';
 
 
 class LandingSplash extends Component {
     render() {
         return (
             <div className="landing-splash-container">
-                <div className="intro-section">
+                <div className="intro-image">
+                    <img src={IntroImage} alt="Yao's name in Chinese"></img>
+                </div>
+                <div className="intro-text">
                     hi man<br />
                     yes
                 </div>
