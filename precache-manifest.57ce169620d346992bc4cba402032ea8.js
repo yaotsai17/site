219823be2f7eea0ddd980c20b4b333a0dcc7a5db
yaotsai17 <yaotsai17@gmail.com>
2019-02-17 07:01:38 -0800

    Updates

---
+++ precache-manifest.57ce169620d346992bc4cba402032ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d39992f0084f6b6d1a73731b48d55f54",
+    "url": "/site/static/media/bg.d39992f0.png"
+  },
+  {
+    "revision": "c4b54bfd49c02f9eaead",
+    "url": "/site/static/js/runtime~main.c4b54bfd.js"
+  },
+  {
+    "revision": "50ee35df0482af47dc74",
+    "url": "/site/static/js/main.50ee35df.chunk.js"
+  },
+  {
+    "revision": "e63a973966c505d904ca",
+    "url": "/site/static/js/2.e63a9739.chunk.js"
+  },
+  {
+    "revision": "50ee35df0482af47dc74",
+    "url": "/site/static/css/main.cdeda482.chunk.css"
+  },
+  {
+    "revision": "b6321ead54001cef229a99ea082d031d",
+    "url": "/site/index.html"
+  }
+];
